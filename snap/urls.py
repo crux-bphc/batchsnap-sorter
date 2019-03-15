a04b2a1e44be692d611682db9383211d31926e7a
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2019-03-16 01:34:51 +0530

    backend: add url for get_images view

--- snap/urls.py
+++ snap/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from batchsnap.views import get_images
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('image/', get_images)
 ]

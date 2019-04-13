ea7d3b26f094b07325f1607bd097898bdb853fce
Naman Arora <36225548+palindrome69@users.noreply.github.com>
2019-04-13 18:27:28 +0530

    add babel import plugin for antd css import

---
+++ frontend/config-overrides.js
@@ -0,0 +1,14 @@
+const {
+  override,
+  fixBabelImports
+} = require('customize-cra');
+
+module.exports = override(
+
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+
+);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import "./main.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
 
 serviceWorker.unregister();

73b0376feb728f356cd9554fe0877db935438b89
vedant mathur <vedantmathur16@gmail.com>
2022-03-08 15:56:41 +0530

    React version updated

--- frontend/src/components/Downloader.jsx
+++ frontend/src/components/Downloader.jsx
@@ -1,119 +1,110 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
 import { Modal, message, Progress, Icon } from "antd";
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
 
-class Downloader extends Component {
-  constructor() {
-    super();
-    this.state = {
-      progress: 0,
-      zipping: false,
-      cancelDownload: false
-    };
-  }
 
-  componentDidMount() {
-    this.downloadImages();
-  }
+function Downloader() {
+const [progress,setProgress] = useState(0);
+const [zipping,setZipping] = useState(false);
+const [cancelDownload,setCancelDownload] = useState(false);
 
-  downloadImages = async () => {
-    const { links } = this.props;
-    let count = 0;
-    let zips = [];
-    let currentZipSize = 0;
+useEffect(()=>{
+  downloadImages();
+})
+const downloadImages = async() =>{
+  const { links } = props;
+  let count = 0;
+  let zips = [];
+  let currentZipSize = 0;
 
-    let currentZip = JSZip();
+  let currentZip = JSZip();
 
-    for (let l of links) {
-      if (this.state.cancelDownload) {
-        this.close();
-        return;
-      }
+  for (let l of links) {
+    if (cancelDownload) {
+      close();
+      return;
+    }
 
-      let res = null;
-      try {
-        res = await axios.get(l, {
-          responseType: "blob"
-        });
-      } catch (e) {
-        message.error("Could not download Images");
-        break;
-      }
-
-      ++count;
-      currentZipSize += res.data.size;
-
-      if (currentZipSize > 499 * 1024 * 1024 || count % 200 === 0) {
-        zips.push(currentZip);
-        currentZip = JSZip(); // new zip
-        currentZipSize = res.data.size;
-      }
-
-      const imageName = l.split(/^\/images\//)[1];
-      currentZip.file(imageName, res.data, {
-        binary: true
+    let res = null;
+    try {
+      res = await axios.get(l, {
+        responseType: "blob"
       });
-
-      this.setState({ progress: Math.floor((count / links.length) * 100) });
+    } catch (e) {
+      message.error("Could not download Images");
+      break;
     }
 
-    if (Object.keys(currentZip.files).length > 0) {
+    ++count;
+    currentZipSize += res.data.size;
+
+    if (currentZipSize > 499 * 1024 * 1024 || count % 200 === 0) {
       zips.push(currentZip);
+      currentZip = JSZip(); // new zip
+      currentZipSize = res.data.size;
     }
 
-    this.setState({ zipping: true });
+    const imageName = l.split(/^\/images\//)[1];
+    currentZip.file(imageName, res.data, {
+      binary: true
+    });
 
-    for (let zip of zips) {
-      try {
-        let content = await zip.generateAsync({
-          type: "blob"
-        });
-        saveAs(content, `batchsnap-pics-${new Date().getTime()}.zip`);
-      } catch (err) {
-        message.error("Error while zipping!");
-        break;
-      }
-    }
-    message.info("Zipping complete!");
-    this.close();
-  };
-
-  close = () => {
-    this.props.onCompleteOrCancel();
-  };
-
-  render() {
-    return (
-      <Modal
-        visible
-        title="Downloading Images.."
-        onCancel={() => {
-          if (!this.state.zipping) this.setState({ cancelDownload: true });
-        }}
-        footer={null}
-        style={{
-          textAlign: "center"
-        }}
-      >
-        <div>
-          {this.state.progress !== 100 ? (
-            <h3>Please wait while the images are being downloaded.</h3>
-          ) : (
-            <h3>Download Successful!</h3>
-          )}
-          <Progress type="circle" percent={this.state.progress} />
-          {this.state.zipping && (
-            <>
-              <h3>Zipping all your pictures..</h3>
-              <Icon type="loading" />
-            </>
-          )}
-        </div>
-      </Modal>
-    );
+    setProgress(Math.floor((count / links.length) * 100));
   }
+
+  if (Object.keys(currentZip.files).length > 0) {
+    zips.push(currentZip);
+  }
+
+  setZipping(true);
+
+  for (let zip of zips) {
+    try {
+      let content = await zip.generateAsync({
+        type: "blob"
+      });
+      saveAs(content, `batchsnap-pics-${new Date().getTime()}.zip`);
+    } catch (err) {
+      message.error("Error while zipping!");
+      break;
+    }
+  }
+  message.info("Zipping complete!");
+  close();
+}
+const close =()=>{
+  props.onCompleteOrCancel();
+}
+return (
+  <Modal
+    visible
+    title="Downloading Images.."
+    onCancel={() => {
+      if (!zipping) setCancelDownload(true);
+    }}
+    footer={null}
+    style={{
+      textAlign: "center"
+    }}
+  >
+    <div>
+      {progress !== 100 ? (
+        <h3>Please wait while the images are being downloaded.</h3>
+      ) : (
+        <h3>Download Successful!</h3>
+      )}
+      <Progress type="circle" percent={progress} />
+      {zipping && (
+        <>
+          <h3>Zipping all your pictures..</h3>
+          <Icon type="loading" />
+        </>
+      )}
+    </div>
+  </Modal>
+);
 }
 
 export default Downloader;
--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,38 +1,24 @@
 import { Upload, Button, Icon } from "antd";
 import React from "react";
-
-class FileUpload extends React.Component {
-  state = {
-    fileList: []
-  };
-
-  handleUpload = () => {
-    const { fileList } = this.state;
-
-    this.props.onUpload(fileList[0]);
-  };
-
-  render() {
-    const { fileList } = this.state;
+function FileUpload(){
+  const [fileList,setFileList] = useState([]);
+  const handleUpload = () => {
+    props.onUpload(fileList[0]);
+  }
     const props = {
-      onRemove: file => {
-        this.setState(state => {
-          const index = state.fileList.indexOf(file);
-          const newFileList = state.fileList.slice();
-          newFileList.splice(index, 1);
+      onRemove : file => {
+          const index = fileList.indexOf(file);
+          setFileList(fileList.slice().splice(index, 1));
           return {
-            fileList: newFileList
+            fileList: fileList
           };
-        });
-      },
+        },
 
       accept: ".jpg, .jpeg, .png",
       multiple: false,
 
       beforeUpload: file => {
-        this.setState(state => ({
-          fileList: [...state.fileList, file]
-        }));
+        setFileList([...fileList, file]);
         return false;
       },
 
@@ -43,14 +29,14 @@ class FileUpload extends React.Component {
       <div>
         <h3>Upload an Image! It should not be a group picture.</h3>
         <Upload {...props}>
-          <Button disabled={this.state.fileList.length >= 1 ? true : false}>
+          <Button disabled={fileList.length >= 1 ? true : false}>
             <Icon type="upload" />
             Select File
           </Button>
         </Upload>
         <Button
           type="primary"
-          onClick={this.handleUpload}
+          onClick={handleUpload}
           hidden={fileList.length === 0}
           style={{ marginTop: 16 }}
         >
@@ -58,7 +44,6 @@ class FileUpload extends React.Component {
         </Button>
       </div>
     );
-  }
 }
 
 export default FileUpload;
